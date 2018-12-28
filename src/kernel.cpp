4a33f78b656611d1695beca1da628fa06430884c
fshahinfar1 <fshahinfar1@gmail.com>
2018-12-28 15:49:34 +0330

    update kernel with new custom types

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -1,5 +1,8 @@
+#include "types.h"
+
+
 void printf(char * str) {
-    static unsigned short* VIDEOMEM = (unsigned short*)0xb8000;
+    static uint16_t* VIDEOMEM = (uint16_t*)0xb8000;
 
     for (int i=0; str[i] != '\0'; ++i) {
         VIDEOMEM[i] = (VIDEOMEM[i] & 0XFF00) | str[i];
@@ -19,7 +22,7 @@ extern "C" void callConstructors() {
 
 
 extern "C" void kernelMain(const void * multiboot_structure,
-        unsigned int /*multiboot_magic*/) {
+        uint32_t /*multiboot_magic*/) {
     printf("Hello world!");
     while(1);
 }
